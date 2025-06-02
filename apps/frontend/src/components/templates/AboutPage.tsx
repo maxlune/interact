b046f8e95bae3c08a84c64c7588df4d744fdc939
maxlune <85277579+maxlune@users.noreply.github.com>
2025-06-02 14:08:47 +0200

    removed zustand test

--- apps/frontend/src/components/templates/AboutPage.tsx
+++ apps/frontend/src/components/templates/AboutPage.tsx
@@ -1,18 +1,7 @@
-import { useCounterStore } from '../../stores/counterStore.ts';
-
 export const AboutPage = () => {
-  const { count, increment } = useCounterStore();
-
   return (
     <>
       <h1>About</h1>
-      <div>
-        <h3>Test Zustand Store</h3>
-        <p>
-          Counter partagé: <span className="font-bold">{count}</span>
-        </p>
-        <button onClick={increment}>+1 depuis About</button>
-      </div>
     </>
   );
 };
--- apps/frontend/src/components/templates/HomePage.tsx
+++ apps/frontend/src/components/templates/HomePage.tsx
@@ -1,17 +1,8 @@
-import { useCounterStore } from '../../stores/counterStore.ts';
-
 export const HomePage = () => {
-  const { count, increment, decrement, reset } = useCounterStore();
   return (
     <>
       <h1 className="text-red-500">Hello World</h1>
       <p style={{ color: 'var(--test-var-color)' }}>Hello</p>
-      <div className="space-x-2 mb-4">
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-        <button onClick={reset}>Reset</button>
-      </div>
-      <p>Count from Zustand: {count}</p>
     </>
   );
 };
